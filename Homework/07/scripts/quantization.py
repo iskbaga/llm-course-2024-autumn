1a28eca4c5e9f94e1285095b59a11199158dc1be
iskbaga <112892889+iskbaga@users.noreply.github.com>
2024-11-29 23:48:40 +0300

    hw 7

--- Homework/07/scripts/quantization.py
+++ Homework/07/scripts/quantization.py
@@ -6,17 +6,11 @@ from torch import Tensor, CharTensor
 def absmax_quantization(x: Tensor) -> Tuple[float, CharTensor]:
     """
     Выполняет квантование тензора `x` с использованием метода максимального значения по модулю (AbsMax).
-
-    Args:
-        x (Tensor): Входной тензор для квантования.
-
-    Returns:
-        Tuple[float, CharTensor]: Кортеж, содержащий масштабный коэффициент `s` и квантованный тензор `x_q` типа `int8`.
-            - `s` (float): Масштабный коэффициент, использованный для квантования.
-            - `x_q` (CharTensor): Квантованный тензор с значениями типа `int8`.
     """
-    s = <ВАШ КОД>
-    x_q = <ВАШ КОД>
+    s = 127 / x.abs().max().item()
+
+    x_q = (x * s).round().to(torch.int8)
+
     return s, x_q
 
 
@@ -31,7 +25,7 @@ def absmax_dequantization(s: float, x_q: Tensor) -> Tensor:
     Returns:
         Tensor: Восстановленный (деквантованный) тензор с типом `float`.
     """
-    return <ВАШ КОД>
+    return x_q.to(torch.float32) / s
 
 
 def zeropoint_quantization(x: Tensor) -> Tuple[float, int, CharTensor]:
@@ -48,10 +42,14 @@ def zeropoint_quantization(x: Tensor) -> Tuple[float, int, CharTensor]:
             - `z` (int): Смещение (нулевая точка), использованное для квантования.
             - `x_q` (CharTensor): Квантованный тензор с значениями типа `int8`.
     """
-    s = <ВАШ КОД>
-    z = <ВАШ КОД>
-    x_q = <ВАШ КОД>
-    return s, z, x_q
+    x_min = x.min().item()
+    x_max = x.max().item()
+
+    s = 255 / (x_max - x_min)
+    z = round(-s * x_min - 128)
+    x_q = (s * x + z).round().clamp(-128, 127).to(torch.int8)
+
+    return s, int(z), x_q
 
 
 def zeropoint_dequantization(s: float, z: int, x_q: Tensor) -> Tensor:
@@ -66,4 +64,4 @@ def zeropoint_dequantization(s: float, z: int, x_q: Tensor) -> Tensor:
     Returns:
         Tensor: Восстановленный (деквантованный) тензор с типом `float`.
     """
-    return <ВАШ КОД>
+    return (x_q.to(torch.float32) - z) / s
